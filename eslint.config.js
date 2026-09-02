2b80cb6ff02ae9b6226f692c3fef1990f7df89ce
ACE Harish <99960959+ace4o4@users.noreply.github.com>
2026-09-02 21:54:43 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
-ythgnv 4yrhfncv
+
 
 export default tseslint.config(
   { ignores: ["dist", ".output", ".vinxi"] },
